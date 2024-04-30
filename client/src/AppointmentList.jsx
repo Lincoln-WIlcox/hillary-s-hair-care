d8f7548eae480fa8215b16de4f498483e186e48c
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-04-30 13:34:07 -0500

    appointment list lists appointments

--- client/src/AppointmentList.jsx
+++ client/src/AppointmentList.jsx
@@ -3,8 +3,34 @@
 const AppointmentList = ({ appointments }) =>
 {
     return (
-        <>
-        </>
+        <div>
+            {
+                appointments.map(appointment =>
+                {
+                    return (
+                        <div>
+                            <span>{appointment.scheduledDate}</span>
+                            <span>{appointment.stylist.name}</span>
+                            <span>{appointment.customer.name}</span>
+                            <div>
+                                {
+                                    appointment.services.map(service =>
+                                    {
+                                        return (
+                                            <>
+                                                <span>{service.name}</span>
+                                            </>
+                                        )
+                                    }
+                                    )
+                                }
+                            </div>
+                        </div>
+                    )
+                }
+                )
+            }
+        </div>
     )
 }
 
