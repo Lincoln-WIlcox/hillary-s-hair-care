1a0fb0b22e7528a06014a1127db9bb2e82475511
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-02 10:04:37 -0500

    customer list lists customers

--- client/src/components/CustomerList.jsx
+++ client/src/components/CustomerList.jsx
@@ -1,6 +1,11 @@
-const CustomerList = () =>
+const CustomerList = ({ customers }) =>
 {
-    return <>customer list</>
+    return <div>
+        {
+            customers?.map(customer =>
+                <span>{customer.name}</span>)
+        }
+    </div>
 }
 
 export default CustomerList
