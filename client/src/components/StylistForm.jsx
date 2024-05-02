838c1d66ba019691657993e6d97cc7b8903644cb
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-02 09:42:49 -0500

    added button to submit that shows an alert if form is invaldi

--- client/src/components/StylistForm.jsx
+++ client/src/components/StylistForm.jsx
@@ -4,8 +4,25 @@ const StylistForm = () =>
 {
     const [stylistName, setStylistName] = useState("")
 
+    const onSubmitClicked = () =>
+    {
+        if(formIsValid())
+        {
+            
+        } else
+        {
+            window.alert("form is invalid")
+        }
+    }
+
+    const formIsValid = () =>
+    {
+        return stylistName != ""
+    }
+
     return <div>
         <input type="text" placeholder="stylist name" value={stylistName} onChange={(event) => setStylistName(event.target.value)} />
+        <button onClick={onSubmitClicked}>Submit</button>
     </div>
 }
 
