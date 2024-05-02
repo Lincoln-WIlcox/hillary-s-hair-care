65b72d328fade6eb3a7e76dd3ace41561a009adc
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-05-02 09:49:21 -0500

    added service function for posting stylist

--- client/src/services/stylistsServices.js
+++ client/src/services/stylistsServices.js
@@ -8,6 +8,16 @@ export const getActiveStylists = async () =>
     return fetch("/api/stylists?active=true").then(res => res.json())
 }
 
+export const postStylist = async (stylist) =>
+{
+    return fetch("/api/stylists",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(stylist)
+        }).then(res => res.json())
+}
+
 export const deactivateStylist = async (id) =>
 {
     return fetch(`/api/stylists/${id}`,
